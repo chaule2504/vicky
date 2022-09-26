29fde54e54d3c1d79fa700e993a7aef1375a29e7
Ruby <chaule2504@gmail.com>
2022-09-27 09:36:14 +1000

    Add CV

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,190 +1,146 @@
 import * as React from "react"
 import type { HeadFC } from "gatsby"
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
-]
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
+    <div>
+      <h1>YEN VY NGUYEN (VICKY)</h1>
+
+      <ul>
+        <li>Email: Yenvy.nguyen0405@gmail.com</li>
+        <li>Address: Unit 611A, 7-9 Kent Road, Mascot, NSW 2020</li>
+        <li>0478 707 689</li>
       </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
+
+      <h2>CAREER OBJECTIVE</h2>
+
+      <p>Ambitious, hardworking and willing-to-learn graduate seeking for graduate role in Business
+        Analysis area. Equipped with basic knowledge and technical skills in BA. Strong attention to
+        detail, excellent time management and proven active listener.</p>
+
+      <h2>SKILLS SUMMARY</h2>
+
+      <h3>Technical Skills</h3>
+
+      <ul>
+        <li>Proficiency in basic office applications (Word, Excel, PowerPoint, MS Visio,
+          PowToon)</li>
+        <li>Data base: MySQL, SQL</li>
+        <li>Data Visualization: MS Excel, Google Chart,
+          Zoho analytics</li>
+        <li>Business analytic tool: Google Ads, Google
+          analytic</li>
       </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+
+      <h3>Qualitative Skills</h3>
+
+      <ul>
+        <li>Analysis and Problem Solving</li>
+        <li>Oral and written communication Skill</li>
+        <li>Report Writing and Presenting</li>
+        <li>Industry knowledge</li>
+      </ul>
+
+      <h2>EDUCATION</h2>
+
+      <table>
+        <tr>
+          <td>
+            <b>Australia Technical and Management College</b><br />
+            Professional Year (IT)
+          </td>
+
+          <td>
+            2021 – Present
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <b>Federation University (Sydney Campus)</b><br />
+            Master of Business Analysis (Enterprise System)
+          </td>
+
+          <td>2018 – 2020</td>
+        </tr>
+
+        <tr>
+          <td>
+            <b>Western Sydney University, Sydney</b><br />
+            Master of Business Administration
+          </td>
+
+          <td>2015 - 2017</td>
+        </tr>
+      </table>
+
+      <h2>UNIVERSITY PROJECTS</h2>
+
+      <ul>
+        <li>Developed business plan for a small medium sized café in inner-Sydney. This included
+          establishing vision, mission, strategic statement, conducting SWOT analysis, financial
+          plan, marketing plan, risk management plan and backup plan</li>
+        <li>Together with teammate, built a software to management the customer profile for a
+          local company in Sydney which provide the solution for customer to help to improve
+          their business performance. This including communicating with client, establish a set of
+          software specifications, building business process chart, step by step complete the
+          software, testing the software and delivering the final product.</li>
+      </ul>
+
+      <h3>EMPLOYMENT</h3>
+
+      <p>ZURE Pty Ltd,
+        Junior Business Analyst</p>
+
+      <p>April 2022-
+        present</p>
+
+        <ul>
+          <li>Assist backend team to update the website interface including design, function and
+work flow by establishing user stories, use cases in order to improve customer
+experience and create more opportunities for upper sale and cross sale
+
+          </li>
+          <li>Updating and maintaining website: uploading new products on website, updating new
+information and picture for current products. Assist to improve the process by
+discovering and implementing solutions to better streamline current methods and build
+into workflow
+
+          </li>
+          <li>Assisting with Marketing Plan and strategy: doing market research and analyse
+competitors’ strategy about price, promotion and marketing. Generating the report
+with charts and diagrams by using data visualizing tools to support for company
+decision making process
+
+          </li>
+          <li>
+          Regularly analysing customer experience on company website and business processes
+as well as the effectiveness and friendliness of company website interface.
+Implementing relevant Shopify APIs to improve the effectiveness and efficiency of
+current work flow and customer experience
+          </li>
+          <li>
+          Assist in building ZURE FAQ manual for user experience: assist with customer service
+task and record all enquiries from customer. Build the general manual instruction for
+customer service team and recommend any updated required for the website to
+improve the customer satisfaction
+          </li>
+          <li>
+          Customer data base updating
+          </li>
+          <li>
+          Analysing marketing effectiveness by using tools in Google Ads and Google Analytic.
+Tracking lead and conversion rate. Comparing the conversion rate from Google Ads and
+other channel such as YouTube, Social media and direct sale then support for marketing
+budgeting plan
+          </li>
+          <li>
+          Support with company brand name Google Ads campaign by creating, editing and
+uploading four commercial videos every month
+          </li>
+        </ul>
+
+    </div>
   )
 }
 
